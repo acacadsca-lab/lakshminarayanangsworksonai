86f7d1f9b07cde13bdca4746f428ee9c72c1f960
Lakshminarayan <acacadsca@gmail.com>
2026-08-28 23:43:39 +0530

    settings merge updation

--- aichatprojectfinal/aichatprojectfinal/settings.py
+++ aichatprojectfinal/aichatprojectfinal/settings.py
@@ -123,7 +123,6 @@ WSGI_APPLICATION = 'aichatprojectfinal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-<<<<<<< HEAD
 # import os
 
 # DATABASES = {
@@ -147,16 +146,6 @@ DATABASES = {
     }
 }
 
-=======
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
->>>>>>> ef11ffcb4387eff0cb348d7d815ef7c457d87342
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
