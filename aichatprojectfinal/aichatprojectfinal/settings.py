7bde95d44dc61a233c076cdc178537cb3ff1891b
Lakshminarayan <acacadsca@gmail.com>
2026-09-05 15:50:00 +0530

    test

--- aichatprojectfinal/aichatprojectfinal/settings.py
+++ aichatprojectfinal/aichatprojectfinal/settings.py
@@ -186,7 +186,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CSRF_TRUSTED_ORIGINS = [
     'https://*.onrender.com',
-    'https://*.railway.app',
+    'https://*.railway.internal',
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if not DEBUG:
